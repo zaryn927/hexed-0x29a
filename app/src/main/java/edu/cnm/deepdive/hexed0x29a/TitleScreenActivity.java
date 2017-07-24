64b30a6ec41ae953cc3421f6a4162cc2afe50a9c
Sky Link <s.z_link@yahoo.com>
2017-07-24 00:40:50 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/hexed0x29a/TitleScreenActivity.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.hexed0x29a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_title_screen);
+  }
+}

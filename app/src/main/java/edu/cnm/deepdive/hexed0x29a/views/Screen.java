ac6ec8db57aa6c1819d557bf1d1238fe630056d0
Sky Link <s.z_link@yahoo.com>
2017-08-07 13:17:47 -0600

    Scaling fixed using rectilinear distance and changing scaling pivot. Map bounds employed. Artifacts can be drawn on map.

--- app/src/main/java/edu/cnm/deepdive/hexed0x29a/views/Screen.java
+++ app/src/main/java/edu/cnm/deepdive/hexed0x29a/views/Screen.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.hexed0x29a.views;
 
+import android.content.ClipData.Item;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -51,6 +54,8 @@ public class Screen extends SurfaceView implements Callback, Runnable {
   private boolean rightPressed;
   private boolean leftPressed;
 
+  Resources res;
+
 
   public Screen(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -58,7 +63,7 @@ public class Screen extends SurfaceView implements Callback, Runnable {
     holder = getHolder();
     holder.addCallback(this);
 
-    Resources res = this.getResources();
+    res = this.getResources();
     background = (Drawable)res.getDrawable(R.drawable.map);
 
   }
@@ -92,36 +97,59 @@ public class Screen extends SurfaceView implements Callback, Runnable {
 
   @Override
   public void run() {
-    float x = 0;
-    float y = 0;
-    float distFromCenter;
-    float scaleX;
-    float scaleY;
+    double x = 0;
+    double y = 0;
+    double distFromCenter = 0;
+    double scale =distFromCenter / 500 + 1;
+    Bitmap originalBitmap = BitmapFactory.decodeResource(res, R.drawable.temp_map);
+    Bitmap greenGem = BitmapFactory.decodeResource(res, R.drawable.green_gem);
+    Bitmap blueGem = BitmapFactory.decodeResource(res, R.drawable.blue_gem);
+    Bitmap redGem = BitmapFactory.decodeResource(res, R.drawable.red_gem);
+    Bitmap pearl = BitmapFactory.decodeResource(res, R.drawable.pearl);
+    Bitmap crystal = BitmapFactory.decodeResource(res, R.drawable.crystal);
+    Bitmap hourglass = BitmapFactory.decodeResource(res, R.drawable.hourglass);
 
     while(isRunning) {
       if (!holder.getSurface().isValid()) {
         continue;
       }
       Canvas canvas = holder.lockCanvas();
-      float origX = x + canvas.getWidth() / 2; // Still tweaking to offset origin
-      float origY = y + canvas.getHeight() / 2; // Still tweaking to offset origin
 
       if (upPressed) {
-        y += 12 ;
-      } else if (downPressed) {
-        y -= 12;
-      } else if (rightPressed) {
-        x -= 16;
-      } else if (leftPressed) {
-        x += 16;
-      }
-      distFromCenter = (Math.abs(y) > Math.abs(x)) ? Math.abs(y) / 5000 : Math.abs(x) /5000;
-      scaleX = distFromCenter + 1;
-      scaleY = distFromCenter + 1;
-      canvas.translate(x + (canvas.getWidth() / 2), y + (canvas.getHeight() / 2));
-      canvas.scale(scaleX, scaleY, 0, 0);
-      background.draw(canvas);
 
+        if ( y < (originalBitmap.getHeight() - Math.abs(y))) {
+          y += 12 / scale;
+        } else {
+          y -= 12 / scale;
+        }
+      } else if (downPressed) {
+        if ( y > - (originalBitmap.getHeight() - Math.abs(y))) {
+          y -= 12 / scale;
+        } else {
+          y += 12 /scale;
+        }
+      } else if (rightPressed) {
+        if ( x > - (originalBitmap.getWidth() - Math.abs(x))) {
+          x -= 12 / scale;
+        } else {
+          x += 12 / scale;
+        }
+      } else if (leftPressed) {
+        if ( x < (originalBitmap.getWidth() - Math.abs(x))) {
+          x += 12 / scale;
+        } else {
+          x -= 12 / scale;
+        }
+      }
+
+      distFromCenter = Math.abs(x) + Math.abs(y);
+      scale = distFromCenter / 500 + 1;
+
+      canvas.scale((float) scale, (float) scale, canvas.getWidth() / 2, canvas.getHeight() / 2);
+      canvas.translate((float) x + (canvas.getWidth() / 2), (float) y + (canvas.getHeight() / 2));
+      background.draw(canvas);
+      canvas.drawBitmap(greenGem,-2000,-1500,null);
+      canvas.drawBitmap(crystal,1750,-1300,null);
 
       holder.unlockCanvasAndPost(canvas);
     }

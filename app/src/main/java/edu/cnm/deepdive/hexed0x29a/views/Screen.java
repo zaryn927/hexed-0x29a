f7cda0dd5d84bbf0c2cd6388eaae7ca41959b291
Sky Link <s.z_link@yahoo.com>
2017-08-08 16:51:18 -0600

    Beginning database integration. Removed callback interface implementation in Screen

--- app/src/main/java/edu/cnm/deepdive/hexed0x29a/views/Screen.java
+++ app/src/main/java/edu/cnm/deepdive/hexed0x29a/views/Screen.java
@@ -2,30 +2,38 @@ package edu.cnm.deepdive.hexed0x29a.views;
 
 import android.content.Context;
 import android.content.res.Resources;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+
 import android.util.AttributeSet;
-import android.view.SurfaceHolder.Callback;
+
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+
 import edu.cnm.deepdive.hexed0x29a.R;
 import edu.cnm.deepdive.hexed0x29a.entities.Artifact;
 import edu.cnm.deepdive.hexed0x29a.entities.Char;
 import edu.cnm.deepdive.hexed0x29a.entities.Terrain;
+import edu.cnm.deepdive.hexed0x29a.helpers.OrmHelper;
+import java.sql.SQLException;
 
 /**
  * Created by zaryn on 7/31/2017.
  */
 
-public class Screen extends SurfaceView implements Callback, Runnable {
+public class Screen extends SurfaceView implements Runnable {
 
   Drawable background;
   SurfaceHolder holder;
   Thread renderThread = null;
   volatile boolean isRunning = false;
-  Char character;
+
 
   public boolean isUpPressed() {
     return upPressed;
@@ -57,18 +65,39 @@ public class Screen extends SurfaceView implements Callback, Runnable {
   private boolean rightPressed;
   private boolean leftPressed;
 
+  private OrmHelper dbHelper = null;
+  Dao<Char,Integer> characterDao;
+  Dao<Artifact, Integer> artifactDao;
+  Dao<Terrain, Integer> terrainDao;
+
   Resources res;
+
+
 
 
   public Screen(Context context, AttributeSet attrs) {
     super(context, attrs);
     setWillNotDraw(false);
     holder = getHolder();
-    holder.addCallback(this);
+//    holder.addCallback(this);
 
     res = this.getResources();
     background = (Drawable)res.getDrawable(R.drawable.map);
 
+  }
+
+  private synchronized OrmHelper getHelper() {
+    if (dbHelper == null) {
+      dbHelper = OpenHelperManager.getHelper(getContext(), OrmHelper.class);
+    }
+    return dbHelper;
+  }
+
+  private synchronized void releaseHelper() {
+    if (dbHelper != null) {
+      OpenHelperManager.releaseHelper();
+      dbHelper = null;
+    }
   }
 
   public void resume() {
@@ -76,30 +105,16 @@ public class Screen extends SurfaceView implements Callback, Runnable {
     renderThread = new Thread(this);
     renderThread.start();
   }
-  @Override
-  public void surfaceCreated(SurfaceHolder holder) {
-    Canvas canvas = holder.lockCanvas();
-
-    getHolder().unlockCanvasAndPost(canvas);
-  }
-
-  @Override
-  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-  }
-
-  @Override
-  public void surfaceDestroyed(SurfaceHolder holder) {
-
-  }
-
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-  }
 
   @Override
   public void run() {
+    try {
+      characterDao = getHelper().getCharDao();
+      artifactDao = getHelper().getArtifactDao();
+      terrainDao = getHelper().getTerrainDao();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
     double x = 0;
     double y = 0;
     double distFromCenter = 0;
